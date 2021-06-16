4267c4768a24415f8d64cec4c88d22d53b964573
jeonlinh99 <78011696+jeonlinh99@users.noreply.github.com>
2021-06-16 16:20:12 +0700

    sua file db

--- inc/conn.php
+++ inc/conn.php
@@ -1,7 +1,7 @@
 <?php
-$severname=	"localhost";
-$username= "root";
-$pass="";
+$severname=	"mariadb";
+$username= "ec2-user";
+$pass="jeonlinh@";
 $dbname="tunesource";
 $conn= mysqli_connect($severname, $username, $pass, $dbname);
 
