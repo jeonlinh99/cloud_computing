9bf1ff70c1f32445ffb03b107e39e99b76564080
jeonlinh99 <78011696+jeonlinh99@users.noreply.github.com>
2021-07-16 20:18:21 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@ include('includes/conn.php');
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Song</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Add Toy</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
